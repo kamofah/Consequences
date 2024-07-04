f6910c580fb209eb9278cfed3018824eb1cb6283
Kwaku Amofah-Boafo <boafo819@gmail.com>
2024-07-04 02:45:37 -0400

    Initial commit with basic project folder structure

---
+++ consequences/screens/GettingStarted.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const GettingStarted = () => {
+  return (
+    <View>
+      <Text>GettingStarted</Text>
+    </View>
+  )
+}
+
+export default GettingStarted
