dea36f95c07543d2a292f9a2f514b911db68e51e
Kwaku Amofah-Boafo <boafo819@gmail.com>
2024-07-31 01:24:04 -0400

    Added prettier formatting for consistency

---
+++ consequences/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 4,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  quoteProps: 'consistent',
+  printWidth: 80,
+};
